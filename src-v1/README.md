4f5a3d53691edb3ac97a53c8e1534db19df1fa4e
Joao_Guilherme2 <80895578+JoaoG23@users.noreply.github.com>
2022-12-20 07:20:29 -0300

    recriando buscars

---
+++ src-v1/README.md
@@ -0,0 +1,42 @@
+![Open in Codespaces](https://classroom.github.com/assets/open-in-codespaces-abfff4d4e15f9e1bd8274d9a39a0befe03a0632bb0f153d0ec72ff541cedbe34.svg)
+# Encontre seu Destino
+
+ Encontre seu Destino é um projeto para facilitar o processo da viagem a lazer, nosso objetivo é oferecer diversas opções para sua viagem além de
+recomendar os melhores destinos possíveis de acordo com suas preferências.
+
+# Requisitos cumpridos: 30/100
+  - sem api
+  - página de perfil com dados fixos
+
+# Nota de apresentação 85/100
+
+**Importante:** A nota-base do grupo é calculada a partir das porcentagens acima. A nota individual leva em consideração ainda a nota de acompanhamento nas reuniões de grupos, a participação efetiva nas tarefas e commits e os descontos por falta nas reuniões. 
+
+## Comentários da correção
+
+No código que está aqui no master, **login e cadastro continuam sem funcionar**. Da mesma maneira, o perfil carregado está fixo. Tudo isso está diminuindo, e bastante, a nota do grupo.
+
+Boa ideia na página de pesquisa por "cidades". Como são limitadas, talvez seria mais legal se fosse uma caixa de seleção, ao invés de texto livre. 
+
+Não detectei nem mesmo a tentativa de uso da API.
+
+Até hoje não temos instruções aqui no readme sobre qual arquivo deve ser acessado para iniciar o uso da página.
+
+
+## Alunos integrantes da equipe
+
+  * Lucas Alexandre Machado Costa
+* Rafael Vinícius da Silva
+* Arthur Damaceno dos Santos Pereira
+* Joao Paulo Aguiar
+
+## Professores responsáveis
+
+* João Caram Santos de Oliveira
+* Lucas Gabriel da Silva Felix
+
+## Quadro de tarefas
+<https://github.com/orgs/DisciplinasProgramacao/projects/44/views/1>
+
+## Instruções para uso
+Coloque aqui as instruções para acessar a página inicial do projeto (caminho completo do arquivo).
---
+++ src-v1/codigo/JavaScript/destino/database.js
@@ -0,0 +1,43 @@
+const lugares = [
+    {
+        id: 'ba',
+        local: "Thermas Resort",
+        imagem: "https://drive.google.com/uc?export=download&id=1SUYgAIF5v974OyR__pHAlRWLdlxh55sv",
+        desc: "O Machadinho Thermas Resort SPA é uma excelente opção para pessoas que visitam Machadinho, oferecendo um ambiente familiar e várias comodidades que vão tornar a sua estadia mais especial.",
+        estado: "BAIHA",
+        destaque: "O Machadinho Thermas"
+    },
+    {
+        id: 'ma',
+        local: "Marão",
+        imagem: "https://drive.google.com/uc?export=download&id=1SUYgAIF5v974OyR__pHAlRWLdlxh55sv",
+        desc: "O Machadinho Thermas Resort SPA é uma excelente opção para pessoas que visitam Machadinho, oferecendo um ambiente familiar e várias comodidades que vão tornar a sua estadia mais especial.",
+        estado: "Maranhão",
+        destaque: "O Machadinho Thermas"
+    },
+
+    {
+        id: 'sp',
+        local: "Sao paiki",
+        imagem: "https://drive.google.com/uc?export=download&id=1SUYgAIF5v974OyR__pHAlRWLdlxh55sv",
+        desc: "O Machadinho Thermas Resort SPA é uma excelente opção para pessoas que visitam Machadinho, oferecendo um ambiente familiar e várias comodidades que vão tornar a sua estadia mais especial.",
+        estado: "Rio Grande do Sul",
+        destaque: "O Machadinho Thermas"
+    },
+    {
+        id: 'rj',
+        local: "Thermas Resort",
+        imagem: "https://drive.google.com/uc?export=download&id=1SUYgAIF5v974OyR__pHAlRWLdlxh55sv",
+        desc: "O Machadinho Thermas Resort SPA é uma excelente opção para pessoas que visitam Machadinho, oferecendo um ambiente familiar e várias comodidades que vão tornar a sua estadia mais especial.",
+        estado: "Rio Grande do Sul",
+        destaque: "O Machadinho Thermas"
+    },
+    {
+        id: 'rj',
+        local: "Thermas Nova",
+        imagem: "https://drive.google.com/uc?export=download&id=1SUYgAIF5v974OyR__pHAlRWLdlxh55sv",
+        desc: "O Machadinho Thermas Resort SPA é uma excelente opção para pessoas que visitam Machadinho, oferecendo um ambiente familiar e várias comodidades que vão tornar a sua estadia mais especial.",
+        estado: "Rio Grande do Sul",
+        destaque: "O Machadinho Thermas"
+    },
+];
